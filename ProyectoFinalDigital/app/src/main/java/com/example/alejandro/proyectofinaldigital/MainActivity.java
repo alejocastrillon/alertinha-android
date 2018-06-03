a6a4e1dd04afb4011afa9df1cd64cc848fa24a27
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2018-06-03 17:34:24 -0500

    Implementación de la ubicación en el proyecto

---
+++ ProyectoFinalDigital/app/src/main/java/com/example/alejandro/proyectofinaldigital/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.alejandro.proyectofinaldigital;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_LOCATION = 1;
+    Button button;
+    LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        button = (Button)findViewById(R.id.button_location);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+        } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            getLocation();
+        }
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location location2 = locationManager.getLastKnownLocation(LocationManager. PASSIVE_PROVIDER);
+            if (location != null) {
+                Toast.makeText(this, "Latitud: " + String.valueOf(location.getLatitude())+ "\n Longitud: " + String.valueOf(location.getLongitude()), Toast.LENGTH_LONG).show();
+            } else  if (location1 != null) {
+                Toast.makeText(this, "Latitud: " + String.valueOf(location1.getLatitude())+ "\n Longitud: " + String.valueOf(location1.getLongitude()), Toast.LENGTH_LONG).show();
+            } else  if (location2 != null) {
+                Toast.makeText(this, "Latitud: " + String.valueOf(location2.getLatitude())+ "\n Longitud: " + String.valueOf(location2.getLongitude()), Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"No es posible acceder a tu ubicación",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    protected void buildAlertMessageNoGps() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Por favor, habilite el GPS de tu celular")
+                .setCancelable(false)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+}
