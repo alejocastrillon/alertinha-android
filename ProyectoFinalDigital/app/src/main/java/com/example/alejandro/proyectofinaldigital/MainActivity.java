cde1fc42da5f103fa060f05a0b05e848896590d9
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2018-06-04 17:36:09 -0500

    Implementacion de envío de datos entre Android y PHP con Volley

--- ProyectoFinalDigital/app/src/main/java/com/example/alejandro/proyectofinaldigital/MainActivity.java
+++ ProyectoFinalDigital/app/src/main/java/com/example/alejandro/proyectofinaldigital/MainActivity.java
@@ -15,12 +15,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int REQUEST_LOCATION = 1;
     Button button;
     LocationManager locationManager;
+    String iploca = "192.168.1.16";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +48,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
         } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            getLocation();
+            try {
+                getLocation();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private void getLocation() {
+    private void getLocation() throws IOException {
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
         } else {
@@ -50,14 +65,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Location location2 = locationManager.getLastKnownLocation(LocationManager. PASSIVE_PROVIDER);
             if (location != null) {
                 Toast.makeText(this, "Latitud: " + String.valueOf(location.getLatitude())+ "\n Longitud: " + String.valueOf(location.getLongitude()), Toast.LENGTH_LONG).show();
+                sendAlerta(location);
             } else  if (location1 != null) {
                 Toast.makeText(this, "Latitud: " + String.valueOf(location1.getLatitude())+ "\n Longitud: " + String.valueOf(location1.getLongitude()), Toast.LENGTH_LONG).show();
+                sendAlerta(location1);
             } else  if (location2 != null) {
                 Toast.makeText(this, "Latitud: " + String.valueOf(location2.getLatitude())+ "\n Longitud: " + String.valueOf(location2.getLongitude()), Toast.LENGTH_LONG).show();
+                sendAlerta(location2);
             }else{
                 Toast.makeText(this,"No es posible acceder a tu ubicación",Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private  void sendAlerta(final Location location){
+        String url = "http://" + iploca + "/alertinha/controllers/insertAlerta.php";
+        RequestQueue  requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), response.trim(), Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "  Error en la peticion volley: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("latitud", String.valueOf(location.getLatitude()));
+                params.put("longitud", String.valueOf(location.getLongitude()));
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 
     protected void buildAlertMessageNoGps() {
